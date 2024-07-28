be015c4f342fb996ae056bc8bea382445296c56d
Steven Junio <Steven.junio91@gmail.com>
2024-07-27 18:11:16 -0700

    Specials

--- src/components/FoodItemCard/FoodItemCard.js
+++ src/components/FoodItemCard/FoodItemCard.js
@@ -1,4 +1,6 @@
-import "./foodItemCard.css"
+import "./foodItemCard.css";
+
+
 
 export default function FoodItemCard() {
     return (
@@ -6,7 +8,15 @@ export default function FoodItemCard() {
             <img src="/images/greek-salad.png" alt="greek salad" />
             <div className="food-card-text-content">
                 <h3>Food Item Card</h3>
-                <p>Food Item Card Description</p>
+                <p>
+                    The famous greek salad of crispy lettuce, peppers, olives and our
+                    Chicago style feta cheese, garnished with crunchy garlic and rosemary
+                    croutons.{" "}
+                </p>
+                <div className="order-delivery-section">
+                    <p className="order-delivery">Order a delivery</p>
+                    <img width={20} src="/images/scooter-delivery.svg" alt="" />
+                </div>
             </div>
         </div>
     );
--- src/components/RestaurantSpecials/RestaurantSpecials.js
+++ src/components/RestaurantSpecials/RestaurantSpecials.js
@@ -1,6 +1,27 @@
 import FoodItemCard from "../FoodItemCard/FoodItemCard";
 import "./restaurantSpecials.css";
 
+
+const specials = [{
+    name: "Greek Salad",
+    description: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+    image: "/images/greek-salad.png",
+    price: 12.99,
+},
+{
+    name: "Brushetta",
+    description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    image: "/images/bruschetta.png",
+    price: 5.99,
+},
+{
+    name: "Lemon Dessert",
+    description: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+    image: "/images/lemon-dessert.png",
+    price: 5.00,
+}
+]
+
 export default function RestaurantSpecials() {
     return (
         <section className="restaurant-specials">
@@ -8,7 +29,16 @@ export default function RestaurantSpecials() {
                 <h2 className="specials-header-text">This weeks specials!</h2>
                 <button className="cta-btn online-menu-btn">Online Menu</button>
             </div>
-            <FoodItemCard />
+            <div className="specials-cards">
+                {specials.map((special, index) => (
+                    <FoodItemCard
+                        key={index}
+                        name={special.name}
+                        description={special.description}
+                        image={special.image}
+                        price={special.price}
+                    />
+                ))}</div>
         </section>
     );
 }
